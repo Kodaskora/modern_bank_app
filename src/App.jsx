7bd9b4e2e3195583c04a7d8186060d4f5faa33ba
kodaskora <tomas.kutniauskis@gmail.com>
2024-10-31 17:50:46 +0100

    finished first styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
-const App = () => {
-  return (
-    <div className='bg-primary w-full overflow-hidden text-cyan-100'>Hello</div>
-  );
-};
+import styles from './style';
+
+const App = () => (
+  <div className='bg-primary w-full overflow-hidden'>
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>Navbar</div>
+    </div>
+
+    <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>Hero</div>
+    </div>
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        Stats Buisness Billing CardDeal Testimonials Clients CTA Footer
+      </div>
+    </div>
+  </div>
+);
 
 export default App;
